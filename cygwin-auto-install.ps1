9f89721456f1846b9480cce958175ef80836de78
Brad Price <bradley.alan.price@gmail.com>
2019-08-17 18:27:41 -0400

    added the zfs package

--- cygwin-auto-install.ps1
+++ cygwin-auto-install.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop'
 $mirror = "http://mirror.vcu.edu/pub/windows/cygwin/"
 $LocalDir = "$env:APPDATA\cygwin"
 $InstallDir = "$env:SystemDrive\cygwin64"
-$Packages = "bsdtar,make,openssh,curl,wget,svn,zip,unzip,vim,screen,python3"
+$Packages = "bsdtar,make,openssh,curl,wget,svn,zip,unzip,vim,screen,python3,zsh"
 
 # Create C:\Temp if it doesnt already exist
 $TempDir = "$env:SystemDrive\Temp"
