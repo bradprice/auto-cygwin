82ee80e9fa4f51dbb3d8fa84415dc541f74f68a5
Brad Price <bradley.alan.price@gmail.com>
2020-05-07 17:36:36 -0400

    added the git and bind-utils packages

--- cygwin-auto-install.ps1
+++ cygwin-auto-install.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop'
 $mirror = "http://mirror.vcu.edu/pub/windows/cygwin/"
 $LocalDir = "$env:APPDATA\cygwin"
 $InstallDir = "$env:SystemDrive\cygwin64"
-$Packages = "bsdtar,make,openssh,curl,wget,svn,zip,unzip,vim,screen,python3,zsh,perl,cpio"
+$Packages = "bsdtar,make,openssh,curl,wget,svn,zip,unzip,vim,screen,python3,zsh,perl,cpio,git,bind-utils"
 
 # Create C:\Temp if it doesnt already exist
 $TempDir = "$env:SystemDrive\Temp"
